b696f8ad3a5b85e2a694a935cd08eee0db9e8321
sangpotado <sangdo1995@gmail.com>
2024-04-25 13:42:38 -0700

    remove_task and cross_row

--- taskList/script.js
+++ taskList/script.js
@@ -5,10 +5,15 @@ function get_row_from_data(checkbox, task_name, due_date, priority) {
     // Create Check Box to be placed under "Done" column.
     const checkboxCell = document.createElement('td');
     checkboxCell.classList.add('check-box');
-    checkboxCell.onclick = remove_task; // Added the click functionality
     const checkboxInput = document.createElement('input');
     checkboxInput.type = 'checkbox';
-                                                                // checkboxInput.checked = checkbox; This was checking a box - commented it out
+    
+    checkboxInput.checked = checkbox;
+    
+//either remove_task or cross_row when checkbox is clicked
+    // checkboxInput.addEventListener('change', remove_task);  
+    checkboxInput.addEventListener('change', cross_row);
+
     checkboxCell.appendChild(checkboxInput);
     row.appendChild(checkboxCell);
 
@@ -75,7 +80,24 @@ function add_task() {
 }
 
 // Mark as Completed //
-function remove_task() {
-    console.log('Box was Checked!');    
-    this.remove();
+// function remove_task() {
+//     console.log('Box was Checked!');    
+//     this.remove();
+// }
+
+function remove_task(event) {
+    const checkbox = event.target;
+    if (checkbox.checked) {
+        const row = checkbox.closest('tr');
+        row.remove();
+    }
+}
+function cross_row(event) {
+    const checkbox = event.target;
+    const row = checkbox.closest('tr');
+    if (checkbox.checked) {
+        row.style.textDecoration = 'line-through'; // Apply line-through style if checkbox is checked
+    }  else {
+        row.style.textDecoration = 'none'; // Remove line-through style if checkbox is unchecked
+    }
 }
